1765cce572c7948258d06688389aa34ec0c2ffa4
Manuel Villegas <manuvill3@gmail.com>
2021-07-29 00:51:04 -0600

    first commit

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page {
+  constructor(private barcodeScanner: BarcodeScanner) {}
+
+  ionViewDidEnter() {
+    console.log('ViewDidEnter');
+    this.onScan();
+  }
+
+  onScan() {
+    this.barcodeScanner
+      .scan()
+      .then((barcodeData) => {
+        console.log('Barcode data', barcodeData);
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
+  }
+}
